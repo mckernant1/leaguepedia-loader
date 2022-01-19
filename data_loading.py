563e3ced43335d949cfa3b00e2cbf654b030b703
Tom McKernan <tmeaglei@gmail.com>
2022-01-18 16:58:22 -0800

    init

---
+++ data_loading.py
@@ -0,0 +1,84 @@
+import boto3
+from leaguepedia_parser.site.leaguepedia import leaguepedia
+from setuptools.namespaces import flatten
+
+ddb = boto3.resource('dynamodb')
+
+leagues_table = ddb.Table('Leagues')
+tournaments_table = ddb.Table('Tournaments')
+games_table = ddb.Table('Games')
+player_table = ddb.Table('Players')
+team_table = ddb.Table('Teams')
+
+
+# https://lol.fandom.com/wiki/Special:CargoTables/Leagues
+def load_leagues_and_return_leages() -> [str]:
+    print('Loading Leagues')
+    res = leaguepedia.query(
+        tables='Leagues',
+        fields='League, League_Short, Region, Level, IsOfficial'
+    )
+    for league in res:
+        league['League_Short'] = league.pop('League Short')
+        leagues_table.put_item(Item=league)
+    return [league['League'] for league in res]
+
+
+# https://lol.fandom.com/wiki/Special:CargoTables/Tournaments
+def load_tourneys_and_return_overview_pages(leagues=None) -> [str]:
+    if leagues is None:
+        leagues = load_leagues_and_return_leages()
+    size = len(leagues)
+    for i, league in enumerate(leagues):
+        print(f'({i}/{size}) Loading Tournaments for {league}')
+        res = leaguepedia.query(
+            tables='Tournaments',
+            fields='League, Name, OverviewPage, DateStart, IsQualifier, IsPlayoffs, IsOfficial',
+            where=f"League='{league}'"
+        )
+
+        for tourney in res:
+            tournaments_table.put_item(Item=tourney)
+        yield [tourney['OverviewPage'] for tourney in res]
+
+
+# https://lol.fandom.com/wiki/Special:CargoTables/ScoreboardGames
+def load_games(overview_pages: [str] = None):
+    if overview_pages is None:
+        overview_pages = list(flatten(load_tourneys_and_return_overview_pages()))
+    size = len(overview_pages)
+    for i, overview_page in enumerate(overview_pages):
+        print(f'({i}/{size}) Loading games for {overview_page}')
+        res = leaguepedia.query(
+            tables='ScoreboardGames',
+            fields='GameId, MatchId, OverviewPage, Tournament, Team1, Team2, VOD, Patch, N_GameInMatch, Gamename, '
+                   'DateTime_UTC',
+            where=f"OverviewPage='{overview_page}'",
+            order_by='DateTime_UTC'
+        )
+        for game in res:
+            games_table.put_item(Item=game)
+
+
+# https://lol.fandom.com/wiki/Special:CargoTables/Players
+def load_players():
+    res = leaguepedia.query(
+        tables='Players',
+        fields='ID, Country, Age, Team, Residency, Role, IsSubstitute'
+    )
+    size = len(res)
+    for i, player in enumerate(res):
+        print(f'({i}/{size}) Loading player {player["ID"]}')
+        player_table.put_item(Item=player)
+
+
+# https://lol.fandom.com/wiki/Special:CargoTables/Teams
+def load_teams():
+    res = leaguepedia.query(
+        tables='Teams',
+        fields='Name, Short, Location, Region, IsDisbanded'
+    )
+    size = len(res)
+    for i, team in enumerate(res):
+        print(f'({i}/{size}) Loading team {team["Name"]}')
+        team_table.put_item(Item=team)
---
+++ load_everything.py
@@ -0,0 +1,39 @@
+from argparse import ArgumentParser
+
+from data_loading import load_players, \
+    load_teams, \
+    load_leagues_and_return_leages, \
+    load_games, \
+    load_tourneys_and_return_overview_pages
+
+
+def get_arg_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    leagues = subparsers.add_parser('leagues')
+    leagues.set_defaults(func=load_leagues_and_return_leages)
+
+    tourneys = subparsers.add_parser('tourneys')
+    tourneys.set_defaults(func=load_tourneys_and_return_overview_pages)
+
+    games = subparsers.add_parser('games')
+    games.set_defaults(func=load_games)
+
+    players = subparsers.add_parser('players')
+    players.set_defaults(func=load_players)
+
+    teams = subparsers.add_parser('teams')
+    teams.set_defaults(func=load_teams)
+
+    return parser
+
+
+if __name__ == '__main__':
+    cmd_parser = get_arg_parser()
+    cmd = cmd_parser.parse_args()
+
+    if not hasattr(cmd, 'func'):
+        cmd_parser.print_help()
+    else:
+        cmd.func()
